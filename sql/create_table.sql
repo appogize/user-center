743e6b34e0d4f786ea3bafbddb79eb87431f7203
航飞 张 <3225879907@qq.com>
2023-04-06 19:35:03 +0800

    add 生产环境配置

---
+++ sql/create_table.sql
@@ -0,0 +1,19 @@
+create table if not exists `user-center`.user
+(
+    username     varchar(256)                       null comment '用户昵称',
+    id           bigint auto_increment
+        primary key,
+    userAccount  varchar(256)                       null comment '账号',
+    avatarUrl    varchar(1024)                      null comment '用户头像',
+    gender       tinyint                            null comment '性别',
+    userPassword varchar(512)                       not null comment '密码',
+    phone        varchar(128)                       null comment '电话',
+    email        varchar(512)                       null comment '邮箱',
+    userStatus   int      default 0                 not null comment '0表示正常',
+    createTime   datetime default CURRENT_TIMESTAMP null comment '创建时间',
+    updateTime   datetime default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete     tinyint  default 0                 not null comment '是否删除',
+    userRole     int      default 0                 not null comment '用户角色：普通用户0，管理员1',
+    planetCode   varchar(512)                       null comment '星球编号'
+)
+    comment '用户表';
